e2947e7e40cc92a76e43052d266775d066666df9
felipe-cardielb <43765834+felipe-cardielb@users.noreply.github.com>
2018-10-17 09:03:37 -0500

    examen

---
+++ Examen/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Carro carro = new Carro();
+            carro.Nombre = " Spark ";
+            carro.Color = " Negro ";
+            carro.Marca = " Chevrolet ";
+            carro.Encender();
+            carro.Avanzar();
+            carro.Apagar();
+
+            Barco barco = new Barco();
+            barco.Nombre = " Titanic ";
+            barco.Color = " Blanco ";
+            barco.Marca = " barcos mexicanos";
+            barco.Encender();
+            barco.Avanzar();
+            barco.Apagar();
+
+            Avion avion = new Avion();
+            avion.Nombre = " volador ";
+            avion.Color = " negro ";
+            avion.Marca = " aviomex";
+            avion.Encender();
+            avion.Avanzar();
+            avion.Apagar();
+
+            Bicicleta bicicleta = new Bicicleta();
+            bicicleta.Nombre = " bici ";
+            bicicleta.Color = " verde ";
+            bicicleta.Marca = " bmx ";
+            bicicleta.Encender();
+            bicicleta.Avanzar();
+            bicicleta.Apagar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Examen/Transporte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Transporte
+    {
+       string nombre = " ", color = " ", marca = "";
+        
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+        public string Marca
+        {
+            get { return this.marca; }
+            set { this.marca = value; }
+        }
+        public Transporte(string nombre, string color, string numllantas)
+        {
+            this.nombre = nombre;
+            this.color = color;
+            this.marca = numllantas;
+        }
+        public void Encender()
+        {
+            Console.WriteLine(" el transporte se ha encendido");
+        }
+        public void Avanzar()
+        {
+            Console.WriteLine(" el transporte esta avanzando");
+        }
+        public void Apagar()
+        {
+            Console.WriteLine(" el transporte se ha apagado ");
+        }
+    }
+}
